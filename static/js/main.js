2bffc3e50e914297f5cc111c2c1b6a32f87d4896
Chad Benson <clbsc7@yahoo.com>
2015-01-11 05:35:28 -0800

    Added clickable links to table one, and then dynamically created table2
    Updated index.html with second table for course section output
    Updated main.js with client side scripts to handle course access
    availability. So now user can see all info on course listings and
    decide which course may fulfill requirement. Still need to add back end
    for populating table 2.

--- static/js/main.js
+++ static/js/main.js
@@ -42,14 +42,17 @@ function fillUniversities() {
 function fillDegrees() {
     if (getCollege() !== "Select a community college" &&
         getUniversity() !== "Select a 4-year university") {
-        if ($('#major').is(':empty')) {
-            $('#major').prop('disabled', false);
-            $.get('/majors', function (data) {
-                data.majors.forEach(function (major) {
-                    $('#major').append('<option value="' + major + '">' + major + '</option>');
-                });
+
+        //     if ($('#major').is(':empty')) {
+        document.getElementById('major').innerHTML = "";
+        $('#course-table tbody').empty();
+        $('#major').prop('disabled', false);
+        $.get('/majors', function (data) {
+            data.majors.forEach(function (major) {
+                $('#major').append('<option value="' + major + '">' + major + '</option>');
             });
-        }
+        });
+        //   }
     } else {
         document.getElementById('major').innerHTML = "";
         $('#major').prop('disabled', true);
@@ -57,27 +60,106 @@ function fillDegrees() {
     }
 }
 
+var getIds = function () {
+    var ret;
+    $.ajax({
+        url: '/ids',
+        async: false,
+        success: function (data) {
+            //alert(data);    
+            ret = data;
+        }
+    });
+    return ret;
+};
+
+var addLinks = function (text) {
+    var temp = getIds();
+    //alert("temp "+temp);
+    var a;
+    for (var id in temp) {
+        //alert("id " + id)
+        if (text === temp[id]) {
+            a = text.replace(temp[id], '<a href="#' + getCollege() + '/' + temp[id] + '">' + temp[id] + '</a>');
+            //alert("a " + a);
+            return a;
+        }
+    }
+    return text;
+};
+
+var populateSections = function (college, course) {
+    // get sections form the db and do stuff with them    
+    $.getJSON('/course', {
+    }, function (data) {
+        var title = $('#course-heading').empty();
+        title.text(data.name + ' (' + data.id + ') at ' + data.college);
+        data.sessions.forEach(function (rowData) {
+            var row = $('<tr>');
+            row
+                .append('<td>' + rowData.sesssionNumber + '</td>' +
+                    '<td>' + rowData.meetings + '</td>' +
+                    '<td>' + rowData.capacity + '</td>' +
+                    '<td>' + rowData.teacher + '</td>');
+            $('#section-table tbody').append(row);
+        });
+    });
+    
+};
+    /*
+    $.getJSON('/ids', {
+        college: college,
+        course: course
+    }, function (data) {
+        var row = $('#section-table').empty();
+        var content = $('#section-table tbody').empty();
+        row.append('<th>Colleges</th> <th>Available Sections</th>');
+        data.forEach(function (rowData) {
+            row = $('#section-table');
+            row
+                .append('<tr><td>' + rowData[0] + '</td>' +
+                    '<td>' + rowData[1] + '</td></tr>');
+        });
+    });
+*/
+
+
+window.onhashchange = function (event) {
+    var newHash = location.hash,
+        college = newHash.split('/')[0],
+        course = newHash.split('/')[1];
+
+    if (college && course) {
+        $('#course-table').prop('hidden', true);
+        $('#session-mode').prop('hidden', false);
+        populateSections(college, course);
+    } else {
+        $('#course-table').prop('hidden', false);
+        $('#session-mode').prop('hidden', true);
+    }
+};
+
 function displayTable() {
 
     if (getMajor() === "Select a major") {
         $('#course-table tbody').empty();
     } else {
         $.getJSON('/table', {
-            from: getCollege(),
-            to: getUniversity(),
-            major: getMajor()
+            //from: getCollege(),
+            //to: getUniversity(),
+            //major: getMajor()
         }, function (data) {
+
             var content = $('#course-table tbody'),
                 row;
-            console.log(data);
             content.empty();
-            
             data.forEach(function (rowData) {
-                row = $('<tr>');
+                row = $('#course-table');
                 row
-                    .append('<td class="clickable">' + rowData[0] + '</td>')
-                    .append('<td>' + rowData[1] + '</td>');
-                content.append(row);
+                // ADD LINKS BELOW
+                .append('<tr><td>' + addLinks(rowData[0]) + '</td>' +
+                    '<td>' + rowData[1] + '</td></tr>');
+
             });
         });
     }
